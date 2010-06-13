365b89c1af6d951b38604d505b64fc5925d2fc93
Jacob Stanley <jacob@stanley.io>
2010-06-13 13:39:33 +0800

    Removed the use of templates/heist
    Just keeping it simple while I test out Cappuccino

--- src/Main.hs
+++ src/Main.hs
@@ -1,58 +1,29 @@
-{-# LANGUAGE OverloadedStrings, PackageImports #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System
 import Control.Applicative
-import "monads-fd" Control.Monad.Trans
-import Data.ByteString.Char8 (ByteString)
-import Data.Time.Clock
+import Control.Monad
 import Snap.Http.Server
 import Snap.Types
 import Snap.Util.FileServe
-import Text.Templating.Heist
-import qualified Data.ByteString.Char8 as B
-import qualified Text.XML.Expat.Tree as X
+import System
 
-template :: TemplateState Snap -> ByteString -> Snap ()
-template state name = do
-    maybe pass writeBS =<< renderTemplate state name
-    modifyResponse $ setContentType "text/html"
-
-site :: TemplateState Snap -> Snap ()
-site ts =
-    ifTop (fileServe "static/index.html") <|>
-    fileServe "static"
-
-timeSplice :: Splice Snap
-timeSplice = do
-    time <- liftIO getCurrentTime
-    return $ [X.Text $ B.pack $ show time]
-
-loadError :: String -> String
-loadError str = "Error loading templates\n"++str
+site :: Snap ()
+site = ifTop (fileServe "static/index.html")
+   <|> fileServe "static"
 
 main :: IO ()
 main = do
-    args <- getArgs
-
-    let port = case args of
-                   []  -> 8080
-                   p:_ -> read p
-
-    (origTs, _) <- bindStaticTag .
-                   bindSplice "time" timeSplice
-                   $ emptyTemplateState
-
-    eitherTs <- loadTemplates "templates" origTs
-    let ts = either error id eitherTs
-
-    either (\s -> putStrLn (loadError s) >> exitFailure)
-           (const $ return ())
-           eitherTs
-    
+    port <- liftM parseArgs getArgs
     putStrLn $ "Starting LambdaλSim on port " ++ show port
-
     httpServe "*" port "lambdasim"
         (Just "access.log")
         (Just "error.log")
-        (site ts)
+        site
+
+
+type Port = Int
+
+parseArgs :: [String] -> Port
+parseArgs []    = 8080
+parseArgs (p:_) = read p
