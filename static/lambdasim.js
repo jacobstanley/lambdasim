6ceb6cf5ad4d03c09047c36c695657740652c9bb
Jacob Stanley <jacob@stanley.io>
2010-07-18 00:25:52 +0800

    Removed custom handling of programmatic updates.
    The right combination of handling change/slide events seems to
    do the trick just as well.

--- static/lambdasim.js
+++ static/lambdasim.js
@@ -6,32 +6,31 @@ function mkSlider(name, header, units, min, max, step) {
     var control = $('#' + name + '-slider');
     var label = $('#' + name + '-label');
 
-    var labelDigits = step < 1 ? 1 : 0;
-    var putUrl = '/vessel/' + name + '/';
-
     control.slider({
         min: min, max: max, step: step,
         animate: true, width: 300,
-        slide: onChange,
-        change: onChange,
+        change: function(ev, ui) {
+            labelValue(ui.value);
+        },
+        slide: function(ev, ui) {
+            labelValue(ui.value);
+            $.put('/vessel/' + name + '/' + ui.value);
+        },
     });
 
-    function onChange(ev, ui) {
-        label.text(header + ': ' + ui.value.toFixed(labelDigits) + units);
-
-        if (!control.slider('updating')) {
-            $.put(putUrl + ui.value);
-        }
+    var labelDigits = step < 1 ? 1 : 0;
+    function labelValue(value) {
+        label.text(header + ': ' + value.toFixed(labelDigits) + units);
     }
 
-    control.slider('update', 0);
+    control.slider('value', 0);
 }
 
 function fetchValues() {
     $.get('/vessel', function(v) {
-        $('#speed-slider').slider('update', v.simSpeed);
-        $('#heading-slider').slider('update', v.simHeading);
-        $('#rudder-slider').slider('update', v.simRudder);
+        $('#speed-slider').slider('value', v.simSpeed);
+        $('#heading-slider').slider('value', v.simHeading);
+        $('#rudder-slider').slider('value', v.simRudder);
         setTimeout(fetchValues, 500);
     });
 }
@@ -64,17 +63,4 @@ jQuery.extend({
     delete_: function(url, data, callback, type) {
         return _ajax_request(url, data, callback, type, 'DELETE');
     }
-});
-
-jQuery.fn.extend(jQuery.ui.slider.prototype, {
-    _updating: false,
-    updating: function() {
-        return this._updating;
-    },
-    update: function(value) {
-        if (this._mouseSliding || this._keySliding) return;
-        this._updating = true;
-        this.value(value);
-        this._updating = false;
-    },
 });
