5240880d48c111554ec8bdfaa2113e5fdcb2679a
Jacob Stanley <jacob@stanley.io>
2010-07-18 04:48:10 +0800

    Added Data instances for dimensional types

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Primitives where
 
 import Control.Parallel.Strategies
+import Data.Data
+import Numeric.NumType (Zero, Pos, Neg)
 import Numeric.Units.Dimensional
 import Numeric.Units.Dimensional.Prelude
 import Numeric.Units.Dimensional.NonSI (nauticalMile)
@@ -14,6 +18,14 @@ type Velocity'        = Velocity Double
 
 knot :: Fractional a => Unit DVelocity a
 knot = nauticalMile / hour
+
+deriving instance Typeable3 Dimensional
+deriving instance Typeable  DQuantity
+deriving instance Typeable7 Dim
+deriving instance Typeable  Zero
+deriving instance Typeable1 Pos
+deriving instance Typeable1 Neg
+deriving instance (Typeable v, Typeable d, Data a) => Data (Dimensional v d a)
 
 instance NFData a => NFData (Dimensional v d a) where
   rnf (Dimensional x) = rnf x
