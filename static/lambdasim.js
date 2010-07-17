9776e28c5853208fc442e8e4469eeee97e7e6f84
Jacob Stanley <jacob@stanley.io>
2010-07-17 21:36:49 +0800

    Switch from uki to jquery ui w/ aristo theme
    Hopefully this is the last switch of UI framework. I have a much
    better feeling about this as it appears to be everything I'm looking
    for:
    
      - Looks great because of Aristo theme
      - Layout is relative to other components and so design is low friction
      - Easy to tweak because it is close to standard html/javascript

--- static/lambdasim.js
+++ static/lambdasim.js
@@ -1,84 +1,27 @@
-layout = uki({
-    view: 'Box', rect: '200 100', anchors: 'left top right bottom',
-    id: 'box',
-    childViews: [
-    {
-        view: 'Label', rect: '0 0 1', anchors: 'top',
-        id: 'speed-label',
-    },
-    {
-        view: 'Slider', rect: '0 20 200 0', anchors: 'top',
-        id: 'speed', url: '/vessel/speed',
-        label: 'speed-label', header: 'Speed', units: ' knots',
-        value: 0, values: steps(-5, 20),
-    },
-    {
-        view: 'Label', rect: '0 50 1', anchors: 'top',
-        id: 'heading-label',
-    },
-    {
-        view: 'Slider', rect: '0 70 200 0', anchors: 'top',
-        id: 'heading', url: '/vessel/heading',
-        label: 'heading-label', header: 'Heading', units: '\u00b0T',
-        value: 0, values: steps(0, 360),
-    },
-    {
-        view: 'Label', rect: '0 100 1', anchors: 'top',
-        id: 'rudder-label',
-    },
-    {
-        view: 'Slider', rect: '0 120 200 0', anchors: 'top',
-        id: 'rudder', url: '/vessel/rudder',
-        label: 'rudder-label', header: 'Rudder', units: '\u00b0/s',
-        value: 0, values: steps(-5, 5),
-    },
-    ]
-});
+function mkSlider(name, header, min, max, step, units) {
+    var group = $('#' + name);
+    group.append('<h3 id="' + name + '-label"></h3>');
+    group.append('<div id="' + name + '-slider" class="slider"></div>');
 
-bindSlider('#speed');
-bindSlider('#heading');
-bindSlider('#rudder');
+    var slider = $('#' + name + '-slider');
+    var label = $('#' + name + '-label');
 
-fetchValues();
-layout.attachTo(document.getElementById('content'), '200 0');
+    slider.slider({
+        value: 0, min: min, max: max, step: step,
+        animate: true, width: 300,
+        slide: onChange,
+        change: onChange,
+    });
 
-var updating = false;
-
-function steps(min, max) {
-    var values = [];
-    for (i = min*10; i <= max*10; i++) {
-        values.push(i/10);
+    function onChange(ev, ui) {
+        updateLabel(ui.value);
     }
-    return values;
+
+    var digits = step < 1 ? 1 : 0;
+
+    function updateLabel(value) {
+        label.text(header + ': ' + value.toFixed(digits) + units);
+    }
+
+    updateLabel(0);
 }
-
-function bindSlider(id) {
-    uki(id).bind('change', function() {
-        var v = this.value();
-        uki('#' + this.label).text(this.header + ': ' + v + this.units);
-
-        if (!updating) {
-            uki.ajax({ type: 'PUT', url: this.url + '/' + v });
-        }
-    });
-}
-
-function fetchValues() {
-    uki.getJSON('/vessel', function(v) {
-        updating = true;
-        updateValue('#speed', v.simSpeed);
-        updateValue('#heading', v.simHeading);
-        updateValue('#rudder', v.simRudder);
-        updating = false;
-        setTimeout(fetchValues, 500);
-    });
-}
-
-function updateValue(id, newValue) {
-    uki(id).each(function() {
-        if (this._dragging) { return; }
-        if (newValue == this.value()) return;
-        this.value(newValue).trigger('change');
-    });
-}
-
