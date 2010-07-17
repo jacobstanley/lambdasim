6d4ddfa6c9b073336a702a8a06c432c84315f9d1
Jacob Stanley <jacob@stanley.io>
2010-07-17 19:38:28 +0800

    Improved the way sliders works with uki

--- static/lambdasim.js
+++ static/lambdasim.js
@@ -1,61 +1,67 @@
 layout = uki({
-    view: 'Box',
-    rect: '200 100',
-    anchors: 'left top right bottom',
-    childViews: [{
-        view: 'Button', rect: '50 10 100 24', anchors: 'top',
-        text: 'Lambda\u03bbsim',
-    },
+    view: 'Box', rect: '200 100', anchors: 'left top right bottom',
+    id: 'box',
+    childViews: [
     {
-        view: 'Label', rect: '0 50 1', anchors: 'top',
+        view: 'Label', rect: '0 0 1', anchors: 'top',
         id: 'speed-label',
     },
     {
-        view: 'Slider', rect: '0 70 200 0', anchors: 'top',
+        view: 'Slider', rect: '0 20 200 0', anchors: 'top',
         id: 'speed', url: '/vessel/speed',
         label: 'speed-label', header: 'Speed', units: ' knots',
-        value: 0, min: -5, max: 20,
+        value: 0, values: steps(-5, 20),
     },
     {
-        view: 'Label', rect: '0 90 1', anchors: 'top',
+        view: 'Label', rect: '0 50 1', anchors: 'top',
         id: 'heading-label',
     },
     {
-        view: 'Slider', rect: '0 110 200 0', anchors: 'top',
+        view: 'Slider', rect: '0 70 200 0', anchors: 'top',
         id: 'heading', url: '/vessel/heading',
         label: 'heading-label', header: 'Heading', units: '\u00b0T',
-        value: 0, min: 0, max: 360,
+        value: 0, values: steps(0, 360),
     },
     {
-        view: 'Label', rect: '0 130 1', anchors: 'top',
+        view: 'Label', rect: '0 100 1', anchors: 'top',
         id: 'rudder-label',
     },
     {
-        view: 'Slider', rect: '0 150 200 0', anchors: 'top',
+        view: 'Slider', rect: '0 120 200 0', anchors: 'top',
         id: 'rudder', url: '/vessel/rudder',
         label: 'rudder-label', header: 'Rudder', units: '\u00b0/s',
-        value: 0, min: -5, max: 5,
+        value: 0, values: steps(-5, 5),
     },
     ]
 });
 
+bindSlider('#speed');
+bindSlider('#heading');
+bindSlider('#rudder');
+
+fetchValues();
+layout.attachTo(document.getElementById('content'), '200 0');
+
 var updating = false;
 
+function steps(min, max) {
+    var values = [];
+    for (i = min*10; i <= max*10; i++) {
+        values.push(i/10);
+    }
+    return values;
+}
+
 function bindSlider(id) {
-    uki(id).change(function() {
-        var v = this.value().toFixed(1);
-        var l = uki('#' + this.label);
-        l.text(this.header + ': ' + v + this.units);
+    uki(id).bind('change', function() {
+        var v = this.value();
+        uki('#' + this.label).text(this.header + ': ' + v + this.units);
 
         if (!updating) {
             uki.ajax({ type: 'PUT', url: this.url + '/' + v });
         }
     });
 }
-
-bindSlider('#speed');
-bindSlider('#heading');
-bindSlider('#rudder');
 
 function fetchValues() {
     uki.getJSON('/vessel', function(v) {
@@ -69,12 +75,10 @@ function fetchValues() {
 }
 
 function updateValue(id, newValue) {
-    var oldValue = uki(id).value();
-    if (oldValue != newValue) {
-        uki(id).value(newValue).change();
-    }
+    uki(id).each(function() {
+        if (this._dragging) { return; }
+        if (newValue == this.value()) return;
+        this.value(newValue).trigger('change');
+    });
 }
-
-fetchValues();
-layout.attachTo(window, '200 0');
 
